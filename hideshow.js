8fb1c3db0e62ecfefc470bc24a6c7520b6f8ebdf
Jack Fischer <jfische8@binghamton.edu>
2014-08-01 10:37:01 -0400

    totally working with nice animations thanks to jquery

--- hideshow.js
+++ hideshow.js
@@ -1,39 +1,44 @@
 jQuery(document).ready(function(){
-    jQuery('#showoverall').live('click', function(event) {        
-	jQuery('#overall').show();
-	jQuery('#cc').hide();
-	jQuery('#unicenters').hide();
-	jQuery('#technical').hide();
+	jQuery("<script src=\"http://brian.trump6.com/overall.js\"></script>").appendTo("#overall");
+});
 
+
+jQuery(document).ready(function(){
+    jQuery('#showoverall').live('click', function(event) { 
+	    jQuery("#overall").empty();
+	    jQuery("#cc").empty();
+	    jQuery("#technical").empty();
+	    jQuery("#unicenters").empty();
+	    jQuery("<script src=\"http://brian.trump6.com/overall.js\"></script>").appendTo("#overall");
     });
 });
 
 jQuery(document).ready(function(){
     jQuery('#showcc').live('click', function(event) {        
-	jQuery('#overall').hide();
-	jQuery('#cc').show();
-	jQuery('#unicenters').hide();
-	jQuery('#technical').hide();
-
+	    jQuery("#overall").empty();
+	    jQuery("#cc").empty();
+	    jQuery("#technical").empty();
+	    jQuery("#unicenters").empty();
+	    jQuery("<script src=\"http://brian.trump6.com/cc.js\"></script>").appendTo("#overall");
     });
 });
 
 jQuery(document).ready(function(){
     jQuery('#showunicenters').live('click', function(event) {        
-	jQuery('#overall').hide();
-	jQuery('#cc').hide();
-	jQuery('#unicenters').show();
-	jQuery('#technical').hide();
-
+	    jQuery("#overall").empty();
+	    jQuery("#cc").empty();
+	    jQuery("#technical").empty();
+	    jQuery("#unicenters").empty();
+	    jQuery("<script src=\"http://brian.trump6.com/unicenters.js\"></script>").appendTo("#overall");
     });
 });
 
 jQuery(document).ready(function(){
     jQuery('#showtechnical').live('click', function(event) {        
-	jQuery('#overall').hide();
-	jQuery('#cc').hide();
-	jQuery('#unicenters').hide();
-	jQuery('#technical').show();
-
+	    jQuery("#overall").empty();
+	    jQuery("#cc").empty();
+	    jQuery("#technical").empty();
+	    jQuery("#unicenters").empty();
+	    jQuery("<script src=\"http://brian.trump6.com/technical.js\"></script>").appendTo("#overall");
     });
 });
